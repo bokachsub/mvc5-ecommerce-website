78484604423335586d5d05a92e04428cf4dd158a
bokachsub <bokachsub@gmail.com>
2019-10-31 22:28:58 -0700

    Enabled custom errors via filters, added crawler filter to prevent indexing home page

--- mvc5ecommerce/mvc5ecommerce/App_Start/FilterConfig.cs
+++ mvc5ecommerce/mvc5ecommerce/App_Start/FilterConfig.cs
@@ -8,7 +8,7 @@ namespace mvc5ecommerce
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new CustomExceptionHandler());
             filters.Add(new LogRequestFilter());
         }
     }
